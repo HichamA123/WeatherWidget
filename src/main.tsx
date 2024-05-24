0d7fa352cd565139c1255f5fb5548e56eee87b94
hicham <hicham_13@live.nl>
2024-05-24 02:12:19 +0200

    chore: added documentation

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { ChakraProvider } from '@chakra-ui/react'
+// chakra is the UI component framework
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
